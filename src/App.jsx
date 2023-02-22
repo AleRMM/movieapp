a885718794845002768713d752e1d715092f765d
AleRMM <karen.ramirez.18@hotmail.com>
2023-02-21 22:03:44 -0600

    chore: Proyect base

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import {ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Login from './components/Login';
+
+function App() {
+  
+  const client = new ApolloClient ({
+    cache: new InMemoryCache(),
+    uri: "http://localhost:4000/"
+  })
+
+  return (
+    <ApolloProvider client={client}>
+
+    </ApolloProvider>
+  )
+}
+
+export default App
